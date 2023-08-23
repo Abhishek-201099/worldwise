67060273fb6e13547a7518dcd57bfa714baac0b3
Abhishek Bhattacharjee <48186772+Abhishek-201099@users.noreply.github.com>
2023-08-23 22:50:46 +0530

    fixed css resolve

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,19 +1,22 @@
-import Spinner from './Spinner';
-import styles from './cityList.module.css';
-import CityItem from './CityItem';
-import Message from './Message';
-import { useCities } from '../contexts/CitiesContext';
+import Spinner from "./Spinner";
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-export default function CityList(){
-  const {cities,isLoading}=useCities();
+export default function CityList() {
+  const { cities, isLoading } = useCities();
 
-  if(isLoading) return <Spinner/>
+  if (isLoading) return <Spinner />;
 
-  if(!cities.length) return <Message message='Add a city by clicking a city on the map' />
+  if (!cities.length)
+    return <Message message="Add a city by clicking a city on the map" />;
 
   return (
     <ul className={styles.cityList}>
-      {cities.map(city=><CityItem key={city.id} city={city} />)}
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
     </ul>
   );
 }
